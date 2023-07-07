b867e2b7bfc3be20cdbaf13876e881604f7e89a1
fgbyte <fer.garciabayon@gmail.com>
2023-07-07 11:08:36 -0400

    extends & assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azulOscuro: '#3C475B',
+        grisFondo: '#D9D9D9',
+        verde: '#03D0B6',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': '2.8125rem',
+        'h2': '2.5rem',
+        'h3': '2.1875rem',
+        'base': '1rem',
+        'destaque': '1.5625rem',
+      }
+    },
   },
   plugins: [],
 }
